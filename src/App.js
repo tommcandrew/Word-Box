007eb5ec19d6a9635d3eea438f9fd31bc5c0e042
Samir70home <sbetmouni@talktalk.net>
2019-06-08 09:20:21 +0100

    Make App a class that extends component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
   return (
     <div className="App">
       <header className="App-header">Soon to be a language learning app
