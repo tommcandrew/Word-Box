e3a4d7931af6ee361ab0b05308705a67c866c435
Samir70home <sbetmouni@talktalk.net>
2019-06-15 12:03:31 +0100

    comment out references to rnd verb in test

--- src/Components/TestPage.js
+++ src/Components/TestPage.js
@@ -8,10 +8,13 @@ const rndNoun = (arr) => {
 
 // so far only tense in vocab.js is 'present'
 // nb: person is zero-indexed [1stSing, 2ndSing, 3rdSing, 1stPlur, 2ndPlur, 3rdPlur]
-const rndVerb = (arr, tense, person) => {
-    var i = Math.floor(Math.random()*arr.length);
-    return {english:arr[i][tense][person], foreign: arr[i][tense+'Foreign'][person]}
-}
+
+// ********************
+// currently no use for rndVerb, so commented out
+// const rndVerb = (arr, tense, person) => {
+//     var i = Math.floor(Math.random()*arr.length);
+//     return {english:arr[i][tense][person], foreign: arr[i][tense+'Foreign'][person]}
+// }
 
 // gender is stored as an array [male, female, neuter]
 // not fully implemented, because the gender of nouns is not currently stored
@@ -22,11 +25,12 @@ const rndAdj = (arr, gender) => {
 
 const TestPage = (props) => {  
   var noun1 = rndNoun(props.words.nouns);
-  var verb1 = rndVerb(props.words.verbs, 'present', 2);
+  //currently no use for verb in sentence
+  //var verb1 = rndVerb(props.words.verbs, 'present', 2);
   var adj1 = rndAdj(props.words.adjectives, 0); //masculine because gender not currently defined
-  var englishSentence = 'The '+noun1.english +' '+ verb1.english + ' ' + adj1.english +'.' ;
+  var englishSentence = 'The '+noun1.english +' is '+ adj1.english +'.' ;
   //NB: 'the' needs to match gender too
-  var foreignSentence = 'Ten '+noun1.foreign +' '+ verb1.foreign + ' ' + adj1.foreign +'.' ;
+  var foreignSentence = 'Ten '+noun1.foreign +' je '+ adj1.foreign +'.' ;
   
   if (props.transMode === 'fromEng') {
       var questionSentence = englishSentence;
