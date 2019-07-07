1c6cc6f498cd195363aa10e7ed4003882dd86f78
Samir70home <sbetmouni@talktalk.net>
2019-07-07 17:08:28 +0100

    remove repetition in the way words are chosen

--- src/Components/TestMaker.js
+++ src/Components/TestMaker.js
@@ -1,22 +1,7 @@
-const rndNoun = (arr) => {
-    var i = Math.floor(Math.random()*arr.length);
-    return {english:arr[i].english, foreign:arr[i].foreign, gender:arr[i].gender}
-}
-
-// so far only tense in vocab.js is 'present'
-// nb: person is zero-indexed [1stSing, 2ndSing, 3rdSing, 1stPlur, 2ndPlur, 3rdPlur]
-
-const rndVerb = (arr) => {
-    var i = Math.floor(Math.random()*arr.length);
-    return arr[i];
-}
-
-
-// gender is stored as an array [male, female, neuter]
-const rndAdj = (arr, gender) => {
-    var i = Math.floor(Math.random()*arr.length);
-    return {english:arr[i].english, foreign:arr[i].foreign[gender]}  
-}
+// use this so much, it is just needed for easier access:
+const randInt = (n) => Math.floor(Math.random()*n);
+const capitalise = (w) => w.replace(/^\w/, (c) => c.toUpperCase())
+const rndElement = (arr) => arr[randInt(arr.length)];
 
 // make a random sentence "The noun is adjective"
 // this is Czech specific at the moment. Change it to find the verb to be.
@@ -24,18 +9,14 @@ const theNounIsAdj = (defArt, noun, adj) => {
     var englishSentence = 'The '+noun.english +' is '+ adj.english;
     //NB: 'the' needs to match gender too
     var foreignSentence = capitalise(defArt.foreign[noun.gender]) 
-        +' '+noun.foreign +' je '+ adj.foreign;
+        +' '+noun.foreign +' je '+ adj.foreign[noun.gender];
 
     return {english:englishSentence, foreign:foreignSentence}
 }
 
-// use this so much, it is just needed for easier access:
-const randInt = (n) => Math.floor(Math.random()*n);
-const capitalise = (w) => w.replace(/^\w/, (c) => c.toUpperCase())
-
 // make a random sentence "Pronoun verbing"
 // remember prounouns are listed ['I', 'you', 'he', 'she', 'it', 'we','you','they']
-// while present tense is given as ['am', 'are', 'is', 'are', 'are', 'are']
+// while present tense is given as [1stSing, 2ndSing, 3rdSing, 1stPlur, 2ndPlur, 3rdPlur]
 const pronounVerb = (pronouns, verb) => {
     var person = randInt(8);
     var verbPerson = [0, 1, 2, 2, 2, 3, 4, 5][person]
@@ -46,9 +27,9 @@ const pronounVerb = (pronouns, verb) => {
 }
 
 export const rndSentence = (wList) => {
-    var noun1 = rndNoun(wList.nouns);   
-    var verb1 = rndVerb(wList.verbs);
-    var adj1 = rndAdj(wList.adjectives, noun1.gender);
+    var noun1 = rndElement(wList.nouns);   
+    var verb1 = rndElement(wList.verbs);
+    var adj1 = rndElement(wList.adjectives);
 
     var sentenceType = randInt(2); // change this to constant while developing new sentence test
     switch (sentenceType) {
